9539a2ae3adfbde2b8e7edaeb5b1cb28e7ee4255
Nate <nathanial.lunceford@gmail.com>
2022-07-24 21:39:47 -0500

    Initial Commit

---
+++ BankEncapsulationExercise/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulationExercise
+{
+    internal class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double dep)
+        {
+            balance += dep;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BankEncapsulationExercise;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BankAccount bankAccount = new BankAccount();
+
+        Console.WriteLine("Write how much you are depositing");
+
+        if(double.TryParse(Console.ReadLine(), out double d))
+        {
+            bankAccount.Deposit(d);
+        }
+        else
+        {
+            Console.WriteLine("You did not enter a double, restart program to deposit");
+        }
+
+        Console.WriteLine("Do you want to retrieve your balance? Y for yes, N for no");
+
+        string doTheyWantToDeposit = Console.ReadLine();
+        
+        if(doTheyWantToDeposit == "y" || doTheyWantToDeposit == "Y")
+        {
+            Console.WriteLine($"{bankAccount.GetBalance()}");
+        }
+        else if (doTheyWantToDeposit == "N" || doTheyWantToDeposit == "n")
+        {
+            Console.WriteLine("Okay, have a nice day!");
+        }
+        else
+        {
+            Console.WriteLine("You did not put a y or an n, restart to retrieve balance");
+        }
+    }
+}
